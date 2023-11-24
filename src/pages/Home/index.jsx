00ce17eea1fef39e852ff192a0c95deba45045eb
eddyzera <esilva644@gmail.com>
2023-11-24 09:41:29 -0300

    feat: create banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import './style.css'
-import { Link } from 'react-router-dom'
 import Menu from "../../components/components/Menu";
+import './style.css'
 
 export const Home = () => {
   return (
     <>
       <Menu/>
+      <section className='banner'>
+        <div className='card'>
+          <h2>Esse elit voluptate amet.</h2>
+          <p>Ipsum voluptate veniam eu consectetur proident consequat fugiat fugiat non laboris nisi ad magna est. Quis aliquip velit ea est officia.</p>
+          <button className='card-button'>começar</button>
+        </div>
+      </section>
     </>
   )
 }
